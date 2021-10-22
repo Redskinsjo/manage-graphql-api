f861d988da565a161b5c391f206b16ee6c190411
Jonathan Carnos <jonathancarnos@MacBook-Pro-de-Jonathan.local>
2021-10-22 22:10:26 +0200

    change cors options

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ require("dotenv").config();
 
 app.use(
   cors({
-    origin: "https://manage-mvp.netlify.app",
+    origin: ["https://manage-mvp.netlify.app/", "http://localhost:3000"],
     methods: "*",
     allowedHeaders: ["Authorization", "Content-Type"],
   })
